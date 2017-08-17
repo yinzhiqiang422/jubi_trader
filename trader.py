2f4fa462051e62bba03150e2e0b5e1d533f89ee1
chenjiang <chenjiang@conew.com>
2017-08-17 16:09:35 +0800

    add

--- trader.py
+++ trader.py
@@ -4,47 +4,94 @@
 import requests
 import json
 import time
-from util import gen_signature, gen_nonce, gen_signature2
+from util import gen_signature, gen_nonce
 
 
-base_url = 'https://www.jubi.com'
 
+class JubiTrader(object):
+    def __init__(self, f_conf):
+        self.conf = json.load(open(f_conf, 'r'))
+        self.base_url = 'https://www.jubi.com'
 
-def get_stream_ticker(coin):
-    param = {'coin': coin}
-    url = base_url + '/api/v1/ticker/'
-    req = requests.get(url, data=param)
-    print type(json.loads(req.content))
-    return req.content
+    def get_stream_ticker(self, coin):
+        param = {'coin': coin}
+        url = self.base_url + '/api/v1/ticker/'
+        req = requests.get(url, data=param)
+        print type(json.loads(req.content))
+        return json.loads(req.content)
 
+    def get_depth(self, coin):
+        param = {'coin': coin}
+        url = self.base_url + '/api/v1/depth/'
+        req = requests.get(url, data=param)
+        return json.loads(req.content)
 
-def get_depth(coin):
-    param = {'coin': coin}
-    url = base_url + '/api/v1/depth/'
-    req = requests.get(url, data=param)
-    return json.loads(req.content)
+    def get_orders(self, coin):
+        param = {'coin': coin}
+        url = self.base_url + '/api/v1/orders/'
+        req = requests.get(url, data=param)
+        return json.loads(req.content)
 
+    def get_balance(self):
+        url = self.base_url + '/api/v1/balance/'
+        # pub_key = 'a3yg3-q28yz-e6j1s-1kifd-f3f1y-iyid2-yp4w6'
+        nonce = int(time.time() * 100)
+        signature = gen_signature(nonce=nonce, key=self.conf['pub_key'])
+        param = {'key': self.conf['pub_key'], 'nonce': nonce,
+                 'signature': signature}
+        req = requests.post(url, data=param)
+        return json.loads(req.content)
 
-def get_orders(coin):
-    param = {'coin': coin}
-    url = base_url + '/api/v1/orders/'
-    req = requests.get(url, data=param)
-    return json.loads(req.content)
+    def get_trade_list(self, coin):
+        url = self.base_url + '/api/v1/trade_list/'
+        nonce = int(time.time() * 100)
+        param = {'key': self.conf['pub_key'], 'nonce': nonce,
+                 'coin': coin, 'type': 'all'}
+        signature = gen_signature(key=self.conf['pub_key'], nonce=nonce,
+                                  coin=coin, type='all')
+        param.update({'signature': signature})
+        req = requests.post(url, data=param)
+        return json.loads(req.content)
 
+    def get_trade_view(self, coin, trade_id):
+        url = self.base_url + '/api/v1/trade_view/'
+        nonce = int(time.time() * 100)
+        signature = gen_signature(key=self.conf['pub_key'], nonce=nonce,
+                                  id=trade_id, coin=coin)
+        param = {'key': self.conf['pub_key'], 'nonce': nonce,
+                 'coin': coin, 'id': trade_id, 'signature': signature}
+        req = requests.post(url, data=param)
+        return json.loads(req.content)
 
-def get_balance(key):
-    url = base_url + '/api/v1/balance/'
-    # pub_key = 'a3yg3-q28yz-e6j1s-1kifd-f3f1y-iyid2-yp4w6'
-    nonce = int(time.time() * 100)
-    signature = gen_signature2(nonce=nonce, key=key)
-    param = {'key': key, 'nonce': nonce, 'signature': signature}
-    req = requests.post(url, data=param)
-    return json.loads(req.content)
+    def trade_cance(self, coin, trade_id):
+        url = self.base_url + '/api/v1/trade_cancel/'
+        nonce = int(time.time() * 100)
+        signature = gen_signature(key=self.conf['pub_key'], nonce=nonce,
+                                  id=trade_id, coin=coin)
+        param = {'key': self.conf['pub_key'], 'nonce': nonce,
+                 'coin': coin, 'id': trade_id, 'signature': signature}
+        req = requests.post(url, data=param)
+        return json.loads(req.content)
+
+    def trade_add(self, coin, trade_type, amount, price):
+        url = self.base_url + '/api/v1/trade_add/'
+        nonce = int(time.time() * 100)
+        signature = gen_signature(key=self.conf['pub_key'], nonce=nonce,
+                                  type=trade_type, coin=coin, amount=amount,
+                                  price=price)
+        param = {'key': self.conf['pub_key'], 'nonce': nonce, 'type': trade_type,
+                 'coin': coin, 'amount': amount, 'price': price, 'signature': signature}
+        req = requests.post(url, data=param)
+        return json.loads(req.content)
 
 
 if __name__ == '__main__':
-    # print get_stream_ticker('btc')
-    # print get_depth('btc')
-    # print get_orders('btc')
-    conf = json.load(open('trader_cj.conf', 'r'))
-    print get_balance(conf['pub_key'])
+    trader = JubiTrader('trader_cj.conf')
+    # print trader.get_stream_ticker('ifc')
+    # print trader.get_orders('btc')
+    # print trader.get_balance()
+    # print trader.get_trade_list('ifc')
+    print trader.trade_add('act', 'buy', 10000, 1.6)
+    # print trader.trade_add('ifc', 'sell', 100000, 2.3)
+    # print trader.get_trade_view('ifc', '1211470')
+    # print trader.trade_cance('ifc', '1211470')
--- util.py
+++ util.py
@@ -5,41 +5,20 @@ import hashlib
 import random
 import hmac
 import json
+import urllib
 
 
-conf = json.loads('trader_cj.conf')
+conf = json.load(open('trader_cj.conf', 'r'))
 
 
 def gen_signature(**kargs):
-    order=['amount', 'price', 'type', 'nonce', 'key']
-    # args_str = reduce(lambda x, y: x + '&' + y,
-    #                   map(lambda x: str(x), kargs.values()), '')
+    encode_args = urllib.urlencode(kargs)
+    print 'encode_args', encode_args
     pri_key = conf['pri_key']
     mm = hashlib.md5()
     mm.update(pri_key)
     pri_key_md5 = mm.hexdigest()
-    args_str = ''
-    for kk in order:
-        if kk in kargs:
-            args_str = args_str + '&' + str(kargs.get(kk))
-    args_str = args_str[1:]
-    print 'ttt', hashlib.sha256(args_str).hexdigest()
-    print 'ttt2', hmac.new('ooo', args_str, hashlib.sha256).hexdigest()
-    print 'ttt3', hmac.new(pri_key_md5, args_str, hashlib.sha256).hexdigest()
-    return hmac.new(pri_key_md5, args_str, hashlib.sha256).hexdigest()
-
-
-def gen_signature2(**kargs):
-    v_args = kargs.values()
-    print 'v_args', v_args
-    v_args = sorted(v_args)
-    print 'sort v_args', v_args
-    args_str = reduce(lambda x, y: str(x) + '&' + str(y), v_args, '')
-    pri_key = 'gNhAK-f@WY}-G[D3h-/CppD-k,A]g-EX{NP-3;iji}'
-    mm = hashlib.md5()
-    mm.update(pri_key)
-    pri_key_md5 = mm.hexdigest()
-    return hmac.new(pri_key_md5, args_str, hashlib.sha256).hexdigest()
+    return hmac.new(pri_key_md5, encode_args, hashlib.sha256).hexdigest()
 
 
 def gen_nonce(lens):
@@ -47,12 +26,8 @@ def gen_nonce(lens):
 
 
 if __name__ == '__main__':
-    sig = gen_signature(price=10000, amount=1, type='buy',
+    sig2 = gen_signature(price=10000, amount=1, type='buy',
                         key='5zi7w-4mnes-swmc4-egg9b-f2iqw-396z4-g541b',
                         nonce=141377098123)
-    sig2 = gen_signature2(price=10000, amount=1, type='buy',
-                        key='5zi7w-4mnes-swmc4-egg9b-f2iqw-396z4-g541b',
-                        nonce=141377098123)
-    print 'sig bb', sig
     print 'sig bb2', sig2
     # print gen_nonce(12)
